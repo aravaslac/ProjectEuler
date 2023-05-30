d539e29d3f409c4874bd94fccac754259ed59ad8
Aravaslac <calsavara.nicolas@gmail.com>
2023-05-30 17:55:18 -0300

    Fim do expediente

---
+++ ex0023/Program.cs
@@ -0,0 +1,74 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] primes = Sieve.Primes(28123);
+        List<int> abundantNumbers = new List<int>
+        {
+            12
+        };
+
+        //foreach (int number in primes)
+        //{
+        //    Console.Write($"{number}, ");
+        //}
+
+        for (int i = 14; i < 28123; i++)
+        {
+            if (primes.Contains(i))
+            {
+                continue;
+            }
+            int[] properDivisors = ProperDivisors.Calculate(i);
+            int sum = 0;
+            foreach (int p in properDivisors)
+            {
+                sum += p;
+            }
+            if (sum > i)
+            {
+                abundantNumbers.Add(i);
+            }
+        }
+        //foreach (int number in abundantNumbers)
+        //{
+        //    Console.Write($"{number}, ");
+        //}
+
+        List<int> sumOfTwoAbundants = new List<int>();
+        foreach (int j in abundantNumbers)
+        {
+            Console.WriteLine(j);
+            if (j > (int) 28124 / 2)
+            {
+                break;
+            }
+            for (int k = j; k < 28123 - j; k++)
+            {
+                int sum = j + k;
+                if (!sumOfTwoAbundants.Contains(sum)) 
+                {
+                    sumOfTwoAbundants.Add(sum);
+                    //Console.WriteLine(sum);
+                }
+            }
+            
+        }
+
+
+
+        int sumOfViable = 0;
+        for (int i = 1; i < 28123; i++)
+        {
+            if (!sumOfTwoAbundants.Contains(i))
+            {
+                sumOfViable += i;
+            }
+        }
+
+        Console.WriteLine(sumOfViable);
+        Console.ReadLine();
+    }
+}
---
+++ ex0023/ProperDivisors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public static class ProperDivisors
+    {
+        public static int[] Calculate(int number)
+        {
+            List<int> divisors = new List<int>
+            {
+                1
+            };
+            for (int i = 2; i < number/2 + 1 ; i++)
+            {
+                if (number % i == 0)
+                {
+                    divisors.Add(i);
+                }
+            }
+            return divisors.ToArray();
+        }
+    }
