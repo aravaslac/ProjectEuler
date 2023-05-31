82b5611bbd2c1ec4d4718c95d5c13d7fecad688c
Aravaslac <calsavara.nicolas@gmail.com>
2023-05-30 21:26:11 -0300

    Finished exercise 24.

---
+++ ex0024/Program.cs
@@ -0,0 +1,27 @@
+﻿using ex0024;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> digits = new List<string>
+        {
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+        };
+        
+        IEnumerable<IEnumerable<string>> permutations = Permutations.GetPermutations(digits, 10);
+        List<long> numbers = new List<long>();
+        foreach (var permutation in permutations)
+        {
+            string numberString = "";
+            foreach (string digit in permutation)
+            {
+                numberString += digit;
+            }
+            Console.WriteLine(numberString);
+            numbers.Add(long.Parse(numberString));
+        }
+        numbers.Sort();
+        Console.WriteLine(numbers[999999]);
+    }
+}
