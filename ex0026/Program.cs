db319e675fa1087e0a84d1b383862ca324af3613
Aravaslac <calsavara.nicolas@gmail.com>
2023-06-01 10:01:17 -0300

    Finished exercise 27

---
+++ ex0026/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int denominator = 2; denominator < 1000; denominator++)
+        {
+            //test for terminating decimal
+            int remainder = denominator;
+            while (remainder > 1)
+            {
+                int testNumber = remainder;
+                if (remainder % 2 == 0)
+                {
+                    remainder /= 2;
+                }
+                if (remainder % 5 == 0)
+                {
+                    remainder /= 5;
+                }
+                if (remainder == testNumber)
+                {
+                    break;
+                }
+            }
+            if (remainder == 1)
+            {
+                continue;
+            }
+
+
+            //https://oeis.org/A001913
+
+        }
+    }
+}
---
+++ ex0027/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] primes = Sieve.Primes(1000);
+
+        int bestConsecutivePrimes = 0;
+        int bestA = 0;
+        int bestB = 0;
+        for (int a = -999; a < 1000; a++)
+        {
+            for (int b = -1000; b < 1001; b++)
+            {
+                int consecutivePrimes = 0;
+                bool looper = true;
+                for ( int i = 0; i < 1000; i++)
+                {
+                    if (primes.Contains(i*i + a*i + b))
+                    {
+                        consecutivePrimes++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (consecutivePrimes > bestConsecutivePrimes)
+                {
+                    bestConsecutivePrimes = consecutivePrimes;
+                    bestA = a;
+                    bestB = b;
+                }
+
+                Console.WriteLine($"{a} {b} {consecutivePrimes};   {bestA} {bestB} {bestConsecutivePrimes}");
+            }
+        }
+
+        Console.WriteLine(bestA * bestB);
+        Console.ReadLine();
+    }
+}
