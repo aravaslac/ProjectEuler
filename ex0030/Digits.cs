cdcab197baf327ce9f134c2663ea49e1c51f84aa
Aravaslac <calsavara.nicolas@gmail.com>
2023-06-02 15:56:08 -0300

    Finished exercise 30

---
+++ ex0030/Digits.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Digits
+{
+    public static int[] ToDigits(int number)
+    {
+        List<int> digits = new();
+        while (number > 0)
+        {
+            digits.Insert(0, number % 10);
+            number = (int)(number / 10);
+        }
+        return digits.ToArray();
+    }
+}
---
+++ ex0030/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Through algebra, it's easy to see that all possible candidates have between 2 and 6 digits.
+        int sum = 0;
+        for (int i = 10; i < 1_000_000; i++)
+        {
+            var digits = Digits.ToDigits(i);
+            int remainder = i;
+            foreach (var digit in digits)
+            {
+                remainder -= digit * digit * digit * digit * digit;
+            }
+
+            if (remainder == 0)
+            {
+                sum += i;
+                Console.WriteLine(i);
+            }
+        }
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine(sum);
+    }
+}
