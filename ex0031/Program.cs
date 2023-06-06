a0d1e38be82f7eca8f842c8f745b0ec032bd24f0
Aravaslac <calsavara.nicolas@gmail.com>
2023-06-06 17:41:25 -0300

    .

--- ex0031/Program.cs
+++ ex0031/Program.cs
@@ -2,6 +2,20 @@
 {
     private static void Main(string[] args)
     {
-        
+        Dictionary<int, List<int>> coinAmounts = new Dictionary<int, List<int>>
+        {
+            {1, new List<int>()},
+            {2, new List<int>()},
+            {5, new List<int>()},
+            {10, new List<int>()},
+            {20, new List<int>()},
+            {50, new List<int>()},
+            {100, new List<int>()},
+            {200, new List<int>()}
+        };
+        foreach (var coin in coinAmounts.Keys)
+        {
+            for (int i = 1; i < )
+        }
     }
 }
