93de48adec15a462c5d0844322e0a969b060f372
Aravaslac <calsavara.nicolas@gmail.com>
2023-06-12 18:10:08 -0300

    Finished exercise 36.

---
+++ _library/StringReversal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _library;
+
+public static class StringReversal
+{
+    public static string Reverse(string s)
+    {
+        char[] charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
---
+++ ex0036/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sum = 0;
+        for (int number = 1; number < 1_000_000; number++)
+        {
+            if (number % 10  == 0)
+            {
+                continue;
+            }
+            string numberAsString = number.ToString();
+            int reverseNumber = int.Parse(_library.StringReversal.Reverse(numberAsString));
+            if (reverseNumber != number)
+            {
+                continue;
+            }
+            string binaryRepresentation = Convert.ToString(number,2);
+            if (binaryRepresentation != _library.StringReversal.Reverse(binaryRepresentation))
+            {
+                continue;
+            }
+            Console.WriteLine($"{number} - {binaryRepresentation}");
+            sum += number;
+        }
+        Console.WriteLine("--------------------------");
+        Console.WriteLine(sum);
+    }
+}
