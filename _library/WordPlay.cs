0845686a932aa2e3b0c35393eeadf2384eb44931
Aravaslac <calsavara.nicolas@gmail.com>
2023-06-14 18:09:45 -0300

    Finished problem 42

---
+++ _library/WordPlay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _library;
+
+public class WordPlay
+{
+    private static readonly Dictionary<char, int> _letterValues = new Dictionary<char, int>();
+
+    static WordPlay()
+    {
+        int i = 1;
+        for (char a = 'A'; a <= 'Z'; a++)
+        {
+            _letterValues.Add(a, i);
+            i++;
+        }
+    }
+    
+public static int GetWordValue(string word)
+    {
+        word = word.ToUpper();
+        int value = 0;
+        foreach(char letter in word)
+        {
+            value += _letterValues[letter];
+        }
+        return value;
+    }
+}
