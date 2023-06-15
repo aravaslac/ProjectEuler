fb4111aee48e06ae7f5c7ac4dff4e73c3850446b
Aravaslac <calsavara.nicolas@gmail.com>
2023-06-15 14:42:08 -0300

    Finished problem 45

---
+++ ex0045/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int pentagonalIndex = 165;
+        int hexagonalIndex = 143;
+        long pentagonal = 40755;
+        long hexagonal = 40755;
+        bool looper = true;
+        while (looper)
+        {
+            hexagonalIndex++;
+            hexagonal = (2 * hexagonalIndex - 1) * hexagonalIndex;
+            while (pentagonal < hexagonal)
+            {
+                pentagonalIndex++;
+                pentagonal += 3 * pentagonalIndex - 2;
+                Console.WriteLine($"p({pentagonalIndex}) = {pentagonal} | h({hexagonalIndex}) = {hexagonal}");
+                if (pentagonal == hexagonal)
+                {
+                    looper = false;
+                }
+            }
+        }
+        Console.ReadLine();
+    }
+}
