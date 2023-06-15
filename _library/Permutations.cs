fee25731f8f88201d05d95f2f5a360e58714f6b5
Aravaslac <calsavara.nicolas@gmail.com>
2023-06-15 16:52:23 -0300

    Finished problem 48

--- _library/Permutations.cs
+++ _library/Permutations.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace _library;
 
-namespace ex0024
+public class Permutations
 {
-    public class Permutations
+    static public IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
     {
-        static public IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
-        {
-            if (length == 1) return list.Select(t => new T[] { t });
-            return GetPermutations(list, length - 1)
-                .SelectMany(t => list.Where(o => !t.Contains(o)),
-                    (t1, t2) => t1.Concat(new T[] { t2 }));
-        }
+        if (length == 1) return list.Select(t => new T[] { t });
+        return GetPermutations(list, length - 1)
+            .SelectMany(t => list.Where(o => !t.Contains(o)),
+                (t1, t2) => t1.Concat(new T[] { t2 }));
     }
 }
---
+++ ex0048/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //The problem can be solved using modulo arithmetics
+        //(a + b) (mod m) = a (mod m) + b (mod m)
+        //(a * b) (mod m) = a (mod m) * b (mod m)
+        long sum = 0;
+        long modulo = 10_000_000_000;
+        for (int @base = 1; @base < 1000; @base++)
+        {
+            Console.WriteLine(@base);
+            long product = 1;
+            for (int exponent = 0; exponent < @base; exponent++)
+            {
+                product = (product * @base) % modulo;
+            }
+            sum = (sum + product) % modulo;
+        }
+        Console.WriteLine("--------------------");
+        Console.WriteLine(sum);
+    }
+}
