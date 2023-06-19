9db7f8300dae9d5281c818dcf089a722594cc7b0
Aravaslac <calsavara.nicolas@gmail.com>
2023-06-19 18:37:45 -0300

    .

---
+++ ex0054/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string pokerFilePath = Path.Combine(Environment.CurrentDirectory, @"0054_poker.txt");
+        string[] hands = File.ReadAllLines(pokerFilePath);
+        foreach (string handsItem in hands)
+        {
+            Console.WriteLine(handsItem);
+        }
+    }
+}
