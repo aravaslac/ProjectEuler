76ec1fafca57a9a1af723ed78cea93173de11a46
Aravaslac <calsavara.nicolas@gmail.com>
2023-07-10 10:26:14 -0300

    Finished problem 79

---
+++ ex0079/Program.cs
@@ -0,0 +1,45 @@
+﻿using ex0079;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Only missing digits are 4 and 5 and there are no obvious repeating digits,
+        // so absolute minimum length is 8.
+
+        string filePath = Path.Combine(Environment.CurrentDirectory, @"0079_keylog.txt");
+        string[] logs = File.ReadAllLines(filePath);
+        long password = 0;
+        int digits = 8;
+        long limit = 100_000_000;
+        
+    Looper:
+        while (password < limit)
+        {
+            string passwordString = password.ToString().PadLeft(digits,'0');
+            bool valid = true;
+            foreach (string log in logs)
+            {
+                if (!ValidityChecker.CheckValidity(log, passwordString))
+                {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid)
+            {
+                Console.WriteLine("------------------");
+                Console.WriteLine($"Password: {passwordString}");
+                Environment.Exit(0);
+            }
+
+            password++;
+        }
+
+        Console.WriteLine($"Password has more than {digits} digits.");
+        digits++;
+        limit *= 10;
+        goto Looper;
+    }
+}
---
+++ ex0079/ValidityChecker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex0079
+{
+    internal static class ValidityChecker
+    {
+        internal static bool CheckValidity(string log, string passwordString)
+        {
+            int firstIndex = passwordString.IndexOf(log[0]);
+            if (firstIndex == -1)
+            {
+                return false;
+            }
+            passwordString = passwordString.Substring(firstIndex + 1);
+
+            int secondIndex = passwordString.IndexOf(log[1]);
+            if (secondIndex == -1)
+            {
+                return false;
+            }
+            passwordString = passwordString.Substring(secondIndex + 1);
+
+            return passwordString.Contains(log[2]);
+        }
+    }
+}
