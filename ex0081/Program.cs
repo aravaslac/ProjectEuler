eaf5f0088527a4c29da21a6271bdd09a7c76c3a3
Aravaslac <calsavara.nicolas@gmail.com>
2023-07-10 16:24:32 -0300

    Finished problem 81.

---
+++ ex0081/Program.cs
@@ -0,0 +1,26 @@
+﻿using ex0081;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var pyramid = Pyramid.BuildPyramid();
+        Console.WriteLine(pyramid);
+        for (int i = pyramid.Count - 2; i > -1; i--)
+        {
+            for (int j = 0; j < i + 1; j++)
+            {
+                if (pyramid [i+1][j+1] == 0 || (pyramid[i + 1][j] < pyramid[i + 1][j + 1] && pyramid[i + 1][j] != 0))
+                {
+                    pyramid[i][j] += pyramid[i + 1][j];
+                }
+                else
+                {
+                    pyramid[i][j] += pyramid[i + 1][j + 1];
+                }
+
+            }
+        }
+        Console.WriteLine(pyramid[0][0]);
+    }
+}
---
+++ ex0081/Pyramid.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex0081
+{
+    internal static class Pyramid
+    {
+        internal static List<List<long>> BuildPyramid()
+        {
+            string filePath = Path.Combine(Environment.CurrentDirectory, @"0081_matrix.txt");
+            string[] stringRows = File.ReadAllLines(filePath);
+
+            List<int[]> rows = new List<int[]>();
+            foreach (string stringRow in stringRows)
+            {
+                string[] splitString = stringRow.Split(',');
+                List<int> row = new List<int>();
+                foreach (string piece in splitString)
+                {
+                    row.Add(int.Parse(piece));
+                }
+                rows.Add(row.ToArray());
+            }
+            
+            int size = rows[0].Length;
+            int pyramidSize = 2 * size - 1;
+
+            List<List<long>> pyramid = new List<List<long>>();
+            
+            //Iterate through upper triangular matrix
+            for (int i = 0; i < size; i++)
+            {
+                List<long> rowNumbers = new List<long>();
+                for (int j = 0; j <= i; j++)
+                {
+                    rowNumbers.Add(rows[i - j][j]);
+                }
+                pyramid.Add(rowNumbers);
+            }
+
+            //Iterate through lower triangular matrix
+            for (int i = 0; i < size - 1; i++)
+            {
+                List<long> rowNumbers = new List<long>();
+                for (int j = 0; j < size - i - 1; j++)
+                {
+                    rowNumbers.Add(rows[size - 1 - j][j + i + 1]);
+                }
+                pyramid.Add(rowNumbers);
+            }
+            
+
+            for (int pyramidRow = size; pyramidRow < pyramidSize; pyramidRow++)
+            {
+                while (pyramid[pyramidRow].Count < pyramidRow + 1)
+                {
+                    pyramid[pyramidRow].Add(0);
+                    pyramid[pyramidRow].Insert(0, 0);
+                }
+            }
+
+            return pyramid;
+        }
+    }
+}
